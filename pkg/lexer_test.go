7c5d89eb96b7610b977caf2758f9cb9b3db6a2aa
Fazendaaa <lucas.carotta@outlook.com>
2022-02-28 15:16:13 -0300

    Starting lexer cases

--- pkg/lexer_test.go
+++ pkg/lexer_test.go
@@ -1,1 +1,17 @@
 package samael
+
+import "testing"
+
+func interfaceToProject(filename string, read map[interface{}]interface{}) (result interface{}, fail error) {
+	return result, fail
+}
+
+func TestLex(t *testing.T) {
+	t.Run("default", func(t *testing.T) {
+		value, fail := LexProject("samael", "../test/config/default/", interfaceToProject)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+}
