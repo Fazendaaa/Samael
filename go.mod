df74385b8d95dfb7ef94c11419f32efd687facba
Fazendaaa <lucas.carotta@outlook.com>
2022-03-08 10:31:21 -0300

    Adding CLI related functions

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/Fazendaaa/Samael
 
 go 1.16
 
-require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+require (
+	github.com/spf13/cobra v1.3.0
+	github.com/theckman/yacspin v0.13.12
+	golang.org/x/sys v0.0.0-20220307203707-22a9840ba4d7 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
---
+++ pkg/cmd.go
@@ -0,0 +1,56 @@
+package samael
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func checkPath(path string) error {
+	dir, fail := os.Stat(path)
+
+	if fail != nil {
+		return fmt.Errorf(`invalid path: %s
+	given error: %q`, path, fail)
+	}
+	if !dir.IsDir() {
+		return fmt.Errorf("%q is not a directory", dir.Name())
+	}
+
+	return nil
+}
+
+func validPath(command string) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, params []string) error {
+		if 1 != len(params) {
+			return fmt.Errorf(`missing project path!
+If you want to %s a project in the current path, you can run it again as:
+	shojo %s .
+Otherwise you may try to:
+	shojo %s /absolute/or/relative/project/path`, command, command, command)
+		}
+
+		return checkPath(params[0])
+	}
+}
+
+func validateProjectPath(projectPath string) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, params []string) error {
+		if 1 > len(params) {
+			return fmt.Errorf("requires at least 1 arg(s), only received 0")
+		}
+
+		if "" == projectPath {
+			path, fail := os.Getwd()
+
+			if nil != fail {
+				fmt.Printf("%v;\ncould not read current directory", fail)
+			}
+
+			projectPath = path
+		}
+
+		return checkPath(projectPath)
+	}
+}
